50d2b63b8819e3e7c190a9fff48efa452c7dd359
MarceloOrtiz <marceloortizz@gmail.com>
2023-08-08 22:15:33 -0300

    algo

--- M10_manejodearchivos/clase09_ej2.py
+++ M10_manejodearchivos/clase09_ej2.py
@@ -1,22 +1,24 @@
 import sys
+
 # Comprobación de seguridad, ejecutar sólo si se recibe 3 argumentos
-if len(sys.argv) == 2:
+if len(sys.argv) == 4:
     import datetime
     import os
-    marca_de_tiempo = datetime.datetime.now()
-    marca_de_tiempo = int(datetime.datetime.timestamp(marca_de_tiempo))
 
-    #temperatura = sys.argv[1]
-    #humedad = sys.argv[2]
-    lluvia = sys.argv[1]
-    temperatura = input('Ingrese la temperatura en grados centígrados')
-    humedad = input('Ingrese el porcentaje de humedad')
-    linea = str(marca_de_tiempo) + ',' + temperatura + ',' + humedad + ',' + lluvia
+    x = datetime.datetime.now()
+    x = int(datetime.datetime.timestamp(x))
 
-    logs_lluvia = open('clase09_ej2.csv', 'a')
-    logs_lluvia.write(linea+'\n')
+    temperatura = sys.argv[1]
+    humedad = sys.argv[2]
+    lluvia = sys.argv[3]
+    # temperatura = input('Ingrese la temperatura en grados centígrados')
+    # humedad = input('Ingrese el porcentaje de humedad')
+    linea = str(x) + "," + temperatura + "," + humedad + "," + lluvia
+
+    logs_lluvia = open("clase09_ej2.csv", "a")
+    logs_lluvia.write(linea + "\n")
     logs_lluvia.close()
 
 else:
     print("ERROR: Introdujo una cantidad de argumentos distinta de tres (3)")
-    print('Ejemplo: clase09_ej1.py <temperatura> <humedad> <True o False>')
+    print("Ejemplo: clase09_ej1.py <temperatura> <humedad> <True o False>")
--- M10_manejodearchivos/ejemplo2.py
+++ M10_manejodearchivos/ejemplo2.py
@@ -1,7 +1,15 @@
 import sys
 import datetime
 
-x = datetime.datetime.now()
-a = open("clase06_ej2.csv", "a")
-a.write(x, sys.argv[2], sys.argv[1], sys.argv[3])
-a.close()
+if len(sys.argv) == 4:
+    x = datetime.datetime.now()
+    x = int(datetime.datetime.timestamp(x))
+    a = open("clase09_ej2.csv", "a")
+    fecha = x
+    humedad = sys.argv[2]
+    temperatura = sys.argv[1]
+    llovio = sys.argv[3]
+    a.write(f"{fecha}, {humedad}, {temperatura}, {llovio} \n")
+    a.close()
+else:
+    print("3 arg")
